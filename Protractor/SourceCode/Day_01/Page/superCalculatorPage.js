37290d5a8cb1709cf5cf63741380295ce8091980
huongbui12 <huongbtt1221@gmail.com>
2018-10-10 01:58:58 +0700

    protractor

---
+++ Protractor/SourceCode/Day_01/Page/superCalculatorPage.js
@@ -0,0 +1,5 @@
+export var firstNumber = element(by.model('first'));
+export var secondNumber = element(by.model('second'));
+export var goButton = element(by.id('gobutton'));
+export var latestResult = element(by.binding('latest'));
+
